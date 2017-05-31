e18cc9e1fb5c5dbd31ef6c15f414b3cadbf8efe5
julioton05 <julioton05@gmail.com>
2017-05-30 21:34:46 -0600

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Julio Ch on 30/5/17.
+//  Copyright © 2017 Julio Ch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
